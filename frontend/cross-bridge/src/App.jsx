2ba9055d35ae4813ff8ecf4a16f6f61eb542aa05
Michael Opare Baah <109297033+MichaelOpareBaah@users.noreply.github.com>
2025-12-14 17:18:48 +0000

    Router changes

--- frontend/cross-bridge/src/App.jsx
+++ frontend/cross-bridge/src/App.jsx
@@ -1,33 +1,35 @@
-
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import './App.css'
-import Layout from './layout/Layout'
-import Home from './pages/Home'
-import DemandCourseDetail from './pages/DemandCourseDetail'
-import AboutUs from './pages/AboutUs'
-import Courses from './pages/Courses'
-import ContactUs from './pages/ContactUs'
-import News from './pages/News'
+import { HashRouter, Route, Routes } from "react-router-dom";
+import "./App.css";
+import Layout from "./layout/Layout";
+import Home from "./pages/Home";
+import DemandCourseDetail from "./pages/DemandCourseDetail";
+import AboutUs from "./pages/AboutUs";
+import Courses from "./pages/Courses";
+import ContactUs from "./pages/ContactUs";
+import News from "./pages/News";
 
 function App() {
-
   return (
-    <BrowserRouter>
-    <Routes>
-      <Route path='/' element={<Layout />}>
-      <Route index element={<Home/>}></Route>
-      <Route path='about-us' element={<AboutUs/>}></Route>
-      <Route path='demand-course-detail/:id' element={<DemandCourseDetail />}></Route>
-      <Route path='/courses/demand-course-detail/:id' element={<DemandCourseDetail />}></Route>
-      <Route path='courses' element={<Courses />}></Route>
-       <Route path='contact-us' element={<ContactUs />}></Route>
-       <Route path='alumni-news' element={<News />}></Route>
-
-  
-      </Route>
-    </Routes>
-    </BrowserRouter>
-  )
+    <HashRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />}></Route>
+          <Route path="about-us" element={<AboutUs />}></Route>
+          <Route
+            path="demand-course-detail/:id"
+            element={<DemandCourseDetail />}
+          ></Route>
+          <Route
+            path="/courses/demand-course-detail/:id"
+            element={<DemandCourseDetail />}
+          ></Route>
+          <Route path="courses" element={<Courses />}></Route>
+          <Route path="contact-us" element={<ContactUs />}></Route>
+          <Route path="alumni-news" element={<News />}></Route>
+        </Route>
+      </Routes>
+    </HashRouter>
+  );
 }
 
-export default App
+export default App;
